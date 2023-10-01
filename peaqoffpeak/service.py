d92cc9a6c378473e1679883e7178a2065e49bb63
Magnus Eldén <magnus.elden@outlook.com>
2023-10-01 14:10:19 +0200

    0.1.0

--- peaqoffpeak/service.py
+++ peaqoffpeak/service.py
@@ -5,6 +5,27 @@ from models.networkarea_model import NetWorkAreaModel
 from models.consumptionprofile_rq import GetConsumptionProfileRq
 
 
+# class SvkCache:
+#     """Cache for SVK API"""
+#     VALIDITY_AREAS = 60 * 60 * 24 #24 hours
+#     VALIDITY_CONSUMPTION_PROFILE = 60 * 60 #1 hour
+    
+#     def __init__(self):
+#         self.network_areas = {}
+#         self.consumption_profiles = {}
+
+#     def invalidate(self) -> None:
+#         self.network_areas = {}
+#         self.consumption_profiles = {}
+
+#     def get_network_areas(self) -> list[NetWorkAreaModel]:
+#         if not self.network_areas:
+#             return None
+#         if self.network_areas.keys[0] + self.VALIDITY_AREAS < datetime.now().timestamp():
+#             return None
+#         return self.network_areas
+
+
 async def get_network_areas() -> list[NetWorkAreaModel]:
     """Get all network areas from SVK API"""
     dto = await repo.get_network_areas()
@@ -13,6 +34,13 @@ async def get_network_areas() -> list[NetWorkAreaModel]:
         inst = NetWorkAreaModel.from_dto(d)
         ret.append(inst)
     return ret
+
+
+async def check_network_area_valid(networkareaid:str) -> bool:
+    """Check if network area is valid"""
+    all_areas = await get_network_areas()
+    return networkareaid in [a.networkareaid for a in all_areas if a.isvalid]
+
 
 async def get_consumption_profile(request: GetConsumptionProfileRq) -> list[ConsumptionProfileModel]:
     """Get consumption profile from SVK API"""
