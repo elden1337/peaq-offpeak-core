bc2a47ef76da5fa58d09e743c1ebf5dbc03a0d6b
Magnus Eldén <magnus.elden@outlook.com>
2023-10-01 14:17:08 +0200

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="peaqoffpeak",
-    version="0.1.0",
+    version="0.1.1",
     author="Magnus Eldén",
     description="Partial wrapper for Svk Mimer Api",
     packages=["peaqoffpeak, peaqoffpeak.models"],
